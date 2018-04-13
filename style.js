365513aa4b45f8ca3be25db110d6ab768368aef8
Louis <louis.hsiao@d8ai.com>
2018-04-13 14:13:57 +0800

    remove bodrder

--- style.js
+++ style.js
@@ -19,8 +19,6 @@ let style = StyleSheet.create({
   dateInput: {
     flex: 1,
     height: 40,
-    borderWidth: 1,
-    borderColor: '#aaa',
     alignItems: 'center',
     justifyContent: 'center'
   },
